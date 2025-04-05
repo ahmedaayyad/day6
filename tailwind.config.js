a9c0c7969013d6af5221ed899e164bfa40240dfe
ahmedaayyad <fgvcvcdfg@gmail.com>
2025-04-05 23:16:09 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'bg-white-60': 'rgba(255,255,255,0.6)',
+        'text-black-60': 'rgba(0,0,0,0.6)',
+        'status-occupied': 'rgba(255,95,95,1)',
+        'status-accepted': 'rgba(17,151,23,1)',
+      },
+    },
+  },
+};
